581ba1b87a4d0288c9c311ed065235b78a0e57b6
Fuad Elhasan Irfani <hello@feilvan.art>
2025-12-24 00:05:42 +0700

    #5 accepted (no I won't optimize this)

--- 5.longest-palindromic-substring.ts
+++ 5.longest-palindromic-substring.ts
@@ -5,73 +5,109 @@
  */
 
 // * TESTCASE
-const t = "abbcccba";
+const t = "bc";
 // @lc code=start
 function longestPalindrome(s: string): string {
-  interface subsIndex {
-    start: number;
-    finish: number;
-  }
   const sArr = s.split("");
-  const sArrRev = sArr.toReversed();
-  let longest: subsIndex = { start: 0, finish: 0 };
+  let longest: string[] = [];
 
-  // early return for already palindrome string
-  if (sArr.every((v, i) => v === sArrRev[i])) return sArr.join("");
+  if (isPal(sArr)) {
+    const out = sArr.join("");
+    // console.log(out);
+    return out;
+  }
 
-  function checkNeighbors(start: number, finish: number): subsIndex {
-    // palindrome
-    if (sArr[start] === sArr[finish]) return { start, finish };
-    // initial loop
-    if (finish - start === 2) {
-      // palindrome left & middle
-      if (sArr[start] === sArr[start + 1]) return { start, finish: start + 1 };
-      // palindrome middle & right
-      if (sArr[start + 1] === sArr[finish]) return { start: start + 1, finish };
-    }
-    // not palindrome
-    return { start: start + 1, finish: finish - 1 };
+  function isPal(s: string[]): boolean {
+    const sr = s.toReversed();
+    return s.every((v, i) => v === sr[i]);
   }
 
   sArr.forEach((v, i, r) => {
-    let subs: subsIndex = { start: 0, finish: 0 };
-    let start = Math.max(0, i - 1);
-    let finish = Math.min(r.length - 1, i + 1);
-    let res = checkNeighbors(start, finish);
-    console.log(i, v, res, sArr.slice(res.start, res.finish + 1));
-    while (subs.finish - subs.start < res.finish - res.start) {
-      res = checkNeighbors(start, finish);
-      subs = { start: res.start, finish: res.finish };
+    let run: boolean = true;
+    let leftI: number = i - 1;
+    let rightI: number = i + 1;
+    let currentSub: string[] = [v];
+    let leftSub: string[] = [r[leftI], v];
+    let rightSub: string[] = [v, r[rightI]];
+    let allSub: string[] = [r[leftI], v, r[rightI]];
+    // console.log(currentSub, i, leftSub, rightSub, allSub);
+
+    while (run) {
+      if (currentSub.length > longest.length) {
+        longest.length = 0;
+        longest.push(...currentSub);
+      }
+
+      const left = isPal(leftSub);
+      const right = isPal(rightSub);
+      const all = isPal(allSub);
+
+      // console.log(i, left, right, all, currentSub);
+
+      if (all) {
+        currentSub.unshift(r[leftI]);
+        currentSub.push(r[rightI]);
+        leftSub.push(r[rightI]);
+        rightSub.unshift(r[leftI]);
+        leftI--;
+        rightI++;
+        leftSub.unshift(r[leftI]);
+        rightSub.push(r[rightI]);
+        allSub.unshift(r[leftI]);
+        allSub.push(r[rightI]);
+      } else if (right) {
+        currentSub.push(r[rightI]);
+        leftSub.push(r[rightI]);
+        rightI++;
+        rightSub.push(r[rightI]);
+        allSub.push(r[rightI]);
+      } else if (left) {
+        currentSub.unshift(r[leftI]);
+        rightSub.unshift(r[leftI]);
+        leftI--;
+        leftSub.unshift(r[leftI]);
+        allSub.unshift(r[leftI]);
+      }
+
+      run = left || right || all;
+
+      if (
+        currentSub[0] === undefined ||
+        currentSub[currentSub.length - 1] === undefined
+      )
+        run = false;
     }
-    subs = { start: res.start, finish: res.finish };
-    if (subs.finish - subs.start > longest.finish - longest.start)
-      longest = { start: subs.start, finish: subs.finish };
   });
-  const out: string = sArr.slice(longest.start, longest.finish + 1).join("");
-  console.log(out);
+
+  const out = longest.join("");
+  // console.log(out);
   return out;
 }
 // @lc code=end
 longestPalindrome(t);
 
 /*
-137m07s 26/143 cases passed (N/A)
-        (i'm tired. looks like i can't solve this. i feel so dumb.)
-172m49s 75/143 cases passed (N/A)
+02h17m07s 26/143 cases passed (N/A) (i'm tired. looks like i can't solve this. i feel so dumb.)
+02h52m49s 75/143 cases passed (N/A)
+04h18m35s 143/143 cases passed (1287 ms)
+          Your runtime beats 5.04 % of typescript submissions
+          Your memory usage beats 12.48 % of typescript submissions (65.6 MB)
 
 * START ENLIGHTENMENT
 
 b a a b d
 ---------
-    a     Y
+    a     
   a a     Y
     a b   N
----------
-  a a     Y
-b a a     N
   a a b   N
 ---------
+  a a     
+b a a     N
+  a a b   N
 b a a b   Y
+---------
+b a a b   
 
 * END ENLIGHTENMENT
 */
